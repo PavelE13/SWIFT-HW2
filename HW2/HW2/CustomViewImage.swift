7feeb207290b3abd7f1a2d766b7dd267dd466425
PavelE13 <94640966+PavelE13@users.noreply.github.com>
2024-02-05 00:42:55 +0300

    Add files via upload

---
+++ HW2/HW2/CustomViewImage.swift
@@ -0,0 +1,17 @@
+//
+//  CustomViewImage.swift
+//  HW2
+//
+//  Created by user on 04.02.2024.
+//  Copyright © 2024 user. All rights reserved.
+//
+
+import UIKit
+
+final class CustomViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Friends"
+    }
+}
