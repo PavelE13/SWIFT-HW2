57eaf1e3c2f55055245a58cce44cc6670b7a2a1c
PavelE13 <94640966+PavelE13@users.noreply.github.com>
2024-02-06 09:05:59 +0300

    Update AppDelegate.swift

--- HW2/HW2/AppDelegate.swift
+++ HW2/HW2/AppDelegate.swift
@@ -14,6 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    window = UIWindow(frame: UIScreen.main.bounds)
+    let viewController = UINavigationController(rootViewController: ViewController())
+    self.window?.rootViewController = viewController
+    self.window?.makeKeyAndVisible()
+    return true
+    }
+
+    /*func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         window = UIWindow(frame: UIScreen.main.bounds)
@@ -21,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = UINavigationController(rootViewController: ViewController())
         
         return true
-    }
+    }*/
     
     /*func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
